78e49c0ddba507363d27cd3d1baff9c9f01997f4
Icassell <icassell0717@gmail.com>
2019-08-27 17:04:06 +0800

    Add Audios

---
+++ Client/DemoProject/Assets/Scripts/AudioManager/AudioInUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioInUI : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        AudioController.PlayMusic("BGM0");
+    }
+
+    public void StartBtn()
+    {
+        AudioController.Play("Effect0");
+    }
+
+}
