4ca54ab09c53671b8817ab6b65c89436a0826601
Icassell <icassell0717@gmail.com>
2019-08-28 22:17:19 +0800

    update guide

---
+++ Client/DemoProject/Assets/Scripts/UIcontroller/PlayerGuide.cs
@@ -0,0 +1,55 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGuide : MonoBehaviour
+{
+
+    private Tween tween;
+
+    private int tag;
+    private float moveX;
+    private Vector3 myVec;
+
+    private void Awake()
+    {
+        DOTween.Init();
+        tag = 0;
+        moveX = 0f;
+        myVec = new Vector3(moveX, 0, 0);
+    }
+
+    public void ToLeft()
+    {
+        tag -= 1;
+        if (tag < 0)
+        {
+            tag = 0;
+            return;
+        }
+        else
+        {
+            moveX += 2160f;
+            myVec.x = moveX;
+            tween = transform.DOLocalMove(myVec, 0.2f).SetEase(Ease.Linear);
+        }
+    }
+
+    public void ToRight()
+    {
+        tag += 1;
+        if (tag > 2)
+        {
+            tag = 2;
+            return;
+        }
+        else
+        {
+            moveX -= 2160f;
+            myVec.x = moveX;
+            tween = transform.DOLocalMove(myVec, 0.2f).SetEase(Ease.Linear);
+        }
+
+    }
+}
