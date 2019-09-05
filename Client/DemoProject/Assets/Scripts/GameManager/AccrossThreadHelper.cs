52d06e8fc508ba529d19d2ee471258e9929e7070
fakersaber <785702978@qq.com>
2019-09-05 13:40:14 +0800

    update crotinue

--- Client/DemoProject/Assets/Scripts/GameManager/AccrossThreadHelper.cs
+++ Client/DemoProject/Assets/Scripts/GameManager/AccrossThreadHelper.cs
@@ -16,33 +16,51 @@ public class AccrossThreadHelper : MonoSingleton<AccrossThreadHelper>
     //    StartCoroutine(Done());
     //}
 
-    private void Start()
-    {
-        //初始化委托集合，并开启协程(Update替代协程也可以)
-        StartCoroutine(Done());
-    }
+    //private void Start()
+    //{
+    //    //初始化委托集合，并开启协程(Update替代协程也可以)
+    //    StartCoroutine(Done());
+    //}
 
-    //协程具体实现
-    IEnumerator Done()
+    ////协程具体实现
+    //IEnumerator Done()
+    //{
+    //    while (true)
+    //    {
+    //        lock(thisLock)
+    //        {
+    //            if (delegateList.Count > 0)
+    //            {
+    //                //遍历委托链，依次执行
+    //                foreach (AccrossThreadFunc item in delegateList)
+    //                {
+    //                    item();
+    //                }
+    //                //执行过的委托代码，清空
+    //                delegateList.Clear();
+    //            }
+    //        }
+    //        yield return new WaitForSeconds(0.01f);
+    //    }
+    //}
+
+    private void Update()
     {
-        while (true)
+        lock (thisLock)
         {
-            lock(thisLock)
+            if (delegateList.Count > 0)
             {
-                if (delegateList.Count > 0)
+                //遍历委托链，依次执行
+                foreach (AccrossThreadFunc item in delegateList)
                 {
-                    //遍历委托链，依次执行
-                    foreach (AccrossThreadFunc item in delegateList)
-                    {
-                        item();
-                    }
-                    //执行过的委托代码，清空
-                    delegateList.Clear();
+                    item();
                 }
+                //执行过的委托代码，清空
+                delegateList.Clear();
             }
-            yield return new WaitForSeconds(0.01f);
         }
     }
+
     //公开的添加委托方法
     public void AddDelegate(AccrossThreadFunc func)
     {
